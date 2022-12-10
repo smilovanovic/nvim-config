8c9b15d1d72736c1c20f85e7476173271e8eaa93
Stefan Milovanovic <dazmaj@hotmail.com>
2022-12-10 14:05:11 +0100

    fix: change root path for nvim tree on session

--- lua/stefan/plugins/auto-session.lua
+++ lua/stefan/plugins/auto-session.lua
@@ -1,1 +1,19 @@
-require("auto-session").setup({auto_session_create_enabled = false})
+local status, autosession = pcall(require, "auto-session")
+if not status then
+	return
+end
+
+local nt_status, nvim_tree = pcall(require, "nvim-tree")
+if not nt_status then
+	return
+end
+
+local function restore_nvim_tree()
+	nvim_tree.change_dir(vim.fn.getcwd())
+end
+
+autosession.setup({
+	auto_session_create_enabled = false,
+	pre_restore_cmds = { "NvimTreeClose" },
+	post_restore_cmds = { restore_nvim_tree },
+})
