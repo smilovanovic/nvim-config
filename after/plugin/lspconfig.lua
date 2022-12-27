844a7b80507c4eccf6a76a980c48703bc28ce7f4
Stefan Milovanovic <dazmaj@hotmail.com>
2022-12-27 12:23:52 +0100

    feat: use telescope as much as possible

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -22,11 +22,15 @@ local keymap = vim.keymap -- for conciseness
 local on_attach = function(client, bufnr)
 	-- keybind options
 	local opts = { noremap = true, silent = true, buffer = bufnr }
+	local z
 
 	-- set keybinds
-	keymap.set("n", "gD", "<cmd>Lspsaga peek_definition<CR>", opts) -- got to declaration
-	keymap.set("n", "gd", "<cmd>Lspsaga lsp_finder<CR>", opts) -- see definition and make edits in window
-	keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
+	keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts) -- got to declaration
+	-- keymap.set("n", "gD", "<cmd>Lspsaga peek_definition<CR>", opts) -- got to declaration
+	-- keymap.set("n", "gd", "<cmd>Lspsaga lsp_finder<CR>", opts) -- see definition and make edits in window
+	keymap.set("n", "gD", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- got to declaration
+	keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- see definition and make edits in window
+	keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- go to implementation
 	keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
 	keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
 	keymap.set("n", "<leader>sD", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
