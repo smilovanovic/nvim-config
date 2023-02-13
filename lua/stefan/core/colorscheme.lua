5ef89d5407048956bbb2fe4564f25819c3ed0fe8
Stefan Milovanovic <dazmaj@hotmail.com>
2023-02-13 21:54:42 +0100

    feat: carbonfox theme

--- lua/stefan/core/colorscheme.lua
+++ lua/stefan/core/colorscheme.lua
@@ -1,5 +1,6 @@
 -- set colorscheme with protected call
 -- in case it isn't installed
+
 local gb_status, gruvbox = pcall(require, "gruvbox")
 if gb_status then
 	gruvbox.setup({
@@ -18,10 +19,22 @@ end
 
 local nf_status, nightfox = pcall(require, "nightfox")
 if nf_status then
+	local palette = require("nightfox.palette").load("carbonfox")
 	nightfox.setup({
 		options = {
 			styles = { comments = "italic", keywords = "bold", types = "italic,bold" },
+			dim_inactive = true,
+			inverse = { -- Inverse highlight for different types
+				match_paren = true,
+			},
+		},
+		palettes = {
+			carbonfox = {
+				bg0 = palette.bg1,
+				bg1 = palette.bg0,
+			},
 		},
 	})
-	vim.cmd("colorscheme nightfox")
+	vim.cmd("colorscheme carbonfox")
+	-- vim.cmd("colorscheme nightfox")
 end
